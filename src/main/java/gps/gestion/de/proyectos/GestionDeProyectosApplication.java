d1dab894118eceb136a7dd8d063e554518159320
jalexis10 <63471717+jalexis10@users.noreply.github.com>
2024-10-02 17:15:52 -0500

    segundo

--- src/main/java/gps/gestion/de/proyectos/GestionDeProyectosApplication.java
+++ src/main/java/gps/gestion/de/proyectos/GestionDeProyectosApplication.java
@@ -2,12 +2,20 @@ package gps.gestion.de.proyectos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping; // Import necesario
+import org.springframework.web.bind.annotation.RequestParam; // Import necesario
+import org.springframework.web.bind.annotation.RestController; // Import necesario
 
 @SpringBootApplication
+@RestController // Agregar esta anotación para que la clase sea un controlador REST
 public class GestionDeProyectosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDeProyectosApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World hola jose") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
