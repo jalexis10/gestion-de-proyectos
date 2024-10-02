e79566539f0ec10c6f53fbade9c388e882350fab
jalexis10 <63471717+jalexis10@users.noreply.github.com>
2024-10-02 15:55:20 -0500

    archivos

---
+++ src/main/java/gps/gestion/de/proyectos/GestionDeProyectosApplication.java
@@ -0,0 +1,13 @@
+package gps.gestion.de.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gps/gestion/de/proyectos/GestionDeProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package gps.gestion.de.proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
