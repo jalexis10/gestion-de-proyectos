586c541ec42d653606b98fa8268a1456a5fba46d
jalexis10 <63471717+jalexis10@users.noreply.github.com>
2024-10-12 14:15:08 -0500

    30

--- src/main/java/gps/gestion/de/proyectos/GestionDeProyectosApplication.java
+++ src/main/java/gps/gestion/de/proyectos/GestionDeProyectosApplication.java
@@ -15,7 +15,7 @@ public class GestionDeProyectosApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Si") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "ok") String name) {
 		return String.format("Hello %s!", name);
 	}
 }
